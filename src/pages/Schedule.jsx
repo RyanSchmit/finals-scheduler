67951c7826f7b1d2efad244cf889fa7b30919894
Ryan Schmitt <ryan15schmitt@gmail.com>
2024-06-18 13:09:59 -0400

    Added favicon and schedule for current final exams

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,5 +1,397 @@
 const Schedule = () => {
-  return <h1>Schedule</h1>;
+  return (
+    <>
+      <h1>Schedule</h1>
+      <h2>
+        Monday/Wednesday or Monday/Wednesday/Friday or Wednesday/Friday or
+        Monday/Friday
+      </h2>
+      <h3>Or Classes Meeting 4 or 5 Days Per Week</h3>
+      <table
+        border="1"
+        cellpadding="1"
+        cellspacing="1"
+        summary="Monday/Wednesday or Monday/Wednesday/Friday or Wednesday/Friday or Monday/Friday Or Classes Meeting 4 or 5 Days Per Week"
+      >
+        <thead>
+          <tr>
+            <th scope="col">Class Start Time</th>
+            <th scope="col">Exam Day</th>
+            <th scope="col">Exam Date</th>
+            <th scope="col">Exam Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>7:10am or 7:40am</td>
+            <td>Friday</td>
+            <td>Dec 13</td>
+            <td>7:10am –10:00am</td>
+          </tr>
+          <tr>
+            <td>8:10am</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>7:10am –10:00am</td>
+          </tr>
+          <tr>
+            <td>9:10am</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>7:10am –10:00am</td>
+          </tr>
+          <tr>
+            <td>10:10am</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>10:10am –1:00pm</td>
+          </tr>
+          <tr>
+            <td>11:10am</td>
+            <td>Friday</td>
+            <td>Dec 13</td>
+            <td>10:10am –1:00pm</td>
+          </tr>
+          <tr>
+            <td>12:10pm</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>10:10am –1:00pm</td>
+          </tr>
+          <tr>
+            <td>1:10pm</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>1:10pm –4:00pm</td>
+          </tr>
+          <tr>
+            <td>2:10pm</td>
+            <td>Friday</td>
+            <td>Dec 13</td>
+            <td>1:10pm –4:00pm</td>
+          </tr>
+          <tr>
+            <td>3:10pm</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>1:10pm –4:00pm</td>
+          </tr>
+          <tr>
+            <td>4:10pm or 4:40pm</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>5:10pm or 5:40pm</td>
+            <td>Friday</td>
+            <td>Dec 13</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>6:10pm or 6:40pm</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>7:10pm or 8:10pm</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Tuesday/Thursday Classes</h2>
+      <table
+        border="1"
+        cellpadding="1"
+        cellspacing="1"
+        summary="Tuesday/Thursday Classes"
+      >
+        <thead>
+          <tr>
+            <th scope="col">Class Start Time</th>
+            <th scope="col">Exam Day</th>
+            <th scope="col">Exam Date</th>
+            <th scope="col">Exam Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>7:10am or 7:40am</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>7:10am –10:00am</td>
+          </tr>
+          <tr>
+            <td>8:10am</td>
+            <td>Thursday</td>
+            <td>
+              <p>Dec 12</p>
+            </td>
+            <td>7:10am –10:00am</td>
+          </tr>
+          <tr>
+            <td>9:10am or 9:40am</td>
+            <td>Thursday</td>
+            <td>Dec 12</td>
+            <td>10:10am –1:00pm</td>
+          </tr>
+          <tr>
+            <td>10:10am</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>10:10am –1:00pm</td>
+          </tr>
+          <tr>
+            <td>12:10pm</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>1:10pm –4:00pm</td>
+          </tr>
+          <tr>
+            <td>1:10pm or 1:40pm</td>
+            <td>Thursday</td>
+            <td>Dec 12</td>
+            <td>1:10pm –4:00pm</td>
+          </tr>
+          <tr>
+            <td>2:10pm</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>3:10pm</td>
+            <td>Thursday</td>
+            <td>Dec 12</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>4:10pm or 4:40pm</td>
+            <td>Thursday</td>
+            <td>Dec 12</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>5:10pm or 5:40pm</td>
+            <td>Thursday</td>
+            <td>Dec 12</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>6:10pm or 6:40pm</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>7:10pm</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>8:10pm</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Monday Only Classes</h2>
+      <table
+        border="1"
+        cellpadding="1"
+        cellspacing="1"
+        summary="Monday Only Classes"
+      >
+        <thead>
+          <tr>
+            <th scope="col">Class Start Time</th>
+            <th scope="col">Exam Day</th>
+            <th scope="col">Exam Date</th>
+            <th scope="col">Exam Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>5:10pm or 5:40pm</td>
+            <td>Friday</td>
+            <td>Dec 13</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>6:10pm</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>7:10pm or 8:10pm</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Tuesday Only Classes</h2>
+      <table
+        border="1"
+        cellpadding="1"
+        cellspacing="1"
+        summary="Tuesday Only Classes"
+      >
+        <thead>
+          <tr>
+            <th scope="col">Class Start Time</th>
+            <th scope="col">Exam Day</th>
+            <th scope="col">Exam Date</th>
+            <th scope="col">Exam Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>5:10pm or 5:40pm</td>
+            <td>Thursday</td>
+            <td>Dec 12</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>6:10pm</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>7:10pm</td>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Wednesday Only Classes</h2>
+      <table
+        border="1"
+        cellpadding="1"
+        cellspacing="1"
+        summary="Wednesday Only Classes"
+      >
+        <thead>
+          <tr>
+            <th scope="col">Class Start Time</th>
+            <th scope="col">Exam Day</th>
+            <th scope="col">Exam Date</th>
+            <th scope="col">Exam Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>5:10pm or 5:40pm</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+          <tr>
+            <td>6:10pm</td>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>7:10pm</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>4:10pm –7:00pm</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Thursday Only Classes</h2>
+      <table
+        border="1"
+        cellpadding="1"
+        cellspacing="1"
+        summary="Thursday Only Classes"
+      >
+        <thead>
+          <tr>
+            <th scope="col">Class Start Time</th>
+            <th scope="col">Exam Day</th>
+            <th scope="col">Exam Date</th>
+            <th scope="col">Exam Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>5:10 or 5:40pm</td>
+            <td>Friday</td>
+            <td>Dec 13</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>6:10pm</td>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>7:10pm</td>
+            <td>Friday</td>
+            <td>Dec 13</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Common Final</h2>
+      <table border="1" cellpadding="1" cellspacing="1" summary="Common Final">
+        <thead>
+          <tr>
+            <th scope="col">Exam Day</th>
+            <th scope="col">Exam Date</th>
+            <th scope="col">Exam Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Saturday</td>
+            <td>Dec 7</td>
+            <td>
+              7:10am-10:00am, 10:10am-1pm, 1:10-4:00pm, 4:10pm-7:00pm,
+              7:10pm-10:00pm
+            </td>
+          </tr>
+          <tr>
+            <td>Monday</td>
+            <td>Dec 9</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>Dec 10</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>Dec 11</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>Dec 12</td>
+            <td>7:10pm –10:00pm</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Schedule;
