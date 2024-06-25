f14010ab63e9fedbb062c1be30722e74731af0c4
Ryan Schmitt <ryan15schmitt@gmail.com>
2024-06-25 00:46:23 -0400

    Create environment variables for backend

--- backend/database.js
+++ backend/database.js
@@ -1,2 +1,10 @@
 import mysql from 'mysql2'
 
+require('dotenv').config();
+
+const connection = mysql.createPool({
+    host:  process.env.DB_USERNAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    
+})
