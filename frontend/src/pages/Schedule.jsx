3a194526ce540b5288a3d520a43433159373d17d
Ryan Schmitt <ryan15schmitt@gmail.com>
2024-06-26 15:58:41 -0400

    Created a fetch to get the current schedule

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 class final {
   constructor(ClassStart, ExamDay, ExamDate, ExamTime) {
     this.ClassStart = ClassStart;
@@ -8,9 +10,18 @@ class final {
 }
 
 const Schedule = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/getSchedule")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Loading..."}
       <h1>Schedule</h1>
       <h2>
         Monday/Wednesday or Monday/Wednesday/Friday or Wednesday/Friday or
@@ -19,8 +30,6 @@ const Schedule = () => {
       <h3>Or Classes Meeting 4 or 5 Days Per Week</h3>
       <table
         border="1"
-        cellpadding="1"
-        cellspacing="1"
         summary="Monday/Wednesday or Monday/Wednesday/Friday or Wednesday/Friday or Monday/Friday Or Classes Meeting 4 or 5 Days Per Week"
       >
         <thead>
@@ -36,90 +45,85 @@ const Schedule = () => {
             <td>7:10am or 7:40am</td>
             <td>Friday</td>
             <td>Dec 13</td>
-            <td>7:10am –10:00am</td>
+            <td>7:10am - 10:00am</td>
           </tr>
           <tr>
             <td>8:10am</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>7:10am –10:00am</td>
+            <td>7:10am - 10:00am</td>
           </tr>
           <tr>
             <td>9:10am</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>7:10am –10:00am</td>
+            <td>7:10am - 10:00am</td>
           </tr>
           <tr>
             <td>10:10am</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>10:10am –1:00pm</td>
+            <td>10:10am - 1:00pm</td>
           </tr>
           <tr>
             <td>11:10am</td>
             <td>Friday</td>
             <td>Dec 13</td>
-            <td>10:10am –1:00pm</td>
+            <td>10:10am - 1:00pm</td>
           </tr>
           <tr>
             <td>12:10pm</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>10:10am –1:00pm</td>
+            <td>10:10am - 1:00pm</td>
           </tr>
           <tr>
             <td>1:10pm</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>1:10pm –4:00pm</td>
+            <td>1:10pm - 4:00pm</td>
           </tr>
           <tr>
             <td>2:10pm</td>
             <td>Friday</td>
             <td>Dec 13</td>
-            <td>1:10pm –4:00pm</td>
+            <td>1:10pm - 4:00pm</td>
           </tr>
           <tr>
             <td>3:10pm</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>1:10pm –4:00pm</td>
+            <td>1:10pm - 4:00pm</td>
           </tr>
           <tr>
             <td>4:10pm or 4:40pm</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>5:10pm or 5:40pm</td>
             <td>Friday</td>
             <td>Dec 13</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>6:10pm or 6:40pm</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>7:10pm or 8:10pm</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
         </tbody>
       </table>
 
       <h2>Tuesday/Thursday Classes</h2>
-      <table
-        border="1"
-        cellpadding="1"
-        cellspacing="1"
-        summary="Tuesday/Thursday Classes"
-      >
+      <table border="1" summary="Tuesday/Thursday Classes">
         <thead>
           <tr>
             <th scope="col">Class Start Time</th>
@@ -133,7 +137,7 @@ const Schedule = () => {
             <td>7:10am or 7:40am</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>7:10am –10:00am</td>
+            <td>7:10am - 10:00am</td>
           </tr>
           <tr>
             <td>8:10am</td>
@@ -141,84 +145,79 @@ const Schedule = () => {
             <td>
               <p>Dec 12</p>
             </td>
-            <td>7:10am –10:00am</td>
+            <td>7:10am - 10:00am</td>
           </tr>
           <tr>
             <td>9:10am or 9:40am</td>
             <td>Thursday</td>
             <td>Dec 12</td>
-            <td>10:10am –1:00pm</td>
+            <td>10:10am - 1:00pm</td>
           </tr>
           <tr>
             <td>10:10am</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>10:10am –1:00pm</td>
+            <td>10:10am - 1:00pm</td>
           </tr>
           <tr>
             <td>12:10pm</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>1:10pm –4:00pm</td>
+            <td>1:10pm - 4:00pm</td>
           </tr>
           <tr>
             <td>1:10pm or 1:40pm</td>
             <td>Thursday</td>
             <td>Dec 12</td>
-            <td>1:10pm –4:00pm</td>
+            <td>1:10pm - 4:00pm</td>
           </tr>
           <tr>
             <td>2:10pm</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>3:10pm</td>
             <td>Thursday</td>
             <td>Dec 12</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>4:10pm or 4:40pm</td>
             <td>Thursday</td>
             <td>Dec 12</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>5:10pm or 5:40pm</td>
             <td>Thursday</td>
             <td>Dec 12</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>6:10pm or 6:40pm</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>7:10pm</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>8:10pm</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
         </tbody>
       </table>
 
       <h2>Monday Only Classes</h2>
-      <table
-        border="1"
-        cellpadding="1"
-        cellspacing="1"
-        summary="Monday Only Classes"
-      >
+      <table border="1" summary="Monday Only Classes">
         <thead>
           <tr>
             <th scope="col">Class Start Time</th>
@@ -232,30 +231,25 @@ const Schedule = () => {
             <td>5:10pm or 5:40pm</td>
             <td>Friday</td>
             <td>Dec 13</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>6:10pm</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>7:10pm or 8:10pm</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
         </tbody>
       </table>
 
       <h2>Tuesday Only Classes</h2>
-      <table
-        border="1"
-        cellpadding="1"
-        cellspacing="1"
-        summary="Tuesday Only Classes"
-      >
+      <table border="1" summary="Tuesday Only Classes">
         <thead>
           <tr>
             <th scope="col">Class Start Time</th>
@@ -269,30 +263,25 @@ const Schedule = () => {
             <td>5:10pm or 5:40pm</td>
             <td>Thursday</td>
             <td>Dec 12</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>6:10pm</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>7:10pm</td>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
         </tbody>
       </table>
 
       <h2>Wednesday Only Classes</h2>
-      <table
-        border="1"
-        cellpadding="1"
-        cellspacing="1"
-        summary="Wednesday Only Classes"
-      >
+      <table border="1" summary="Wednesday Only Classes">
         <thead>
           <tr>
             <th scope="col">Class Start Time</th>
@@ -306,30 +295,25 @@ const Schedule = () => {
             <td>5:10pm or 5:40pm</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
           <tr>
             <td>6:10pm</td>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>7:10pm</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>4:10pm –7:00pm</td>
+            <td>4:10pm - 7:00pm</td>
           </tr>
         </tbody>
       </table>
 
       <h2>Thursday Only Classes</h2>
-      <table
-        border="1"
-        cellpadding="1"
-        cellspacing="1"
-        summary="Thursday Only Classes"
-      >
+      <table border="1" summary="Thursday Only Classes">
         <thead>
           <tr>
             <th scope="col">Class Start Time</th>
@@ -343,25 +327,25 @@ const Schedule = () => {
             <td>5:10 or 5:40pm</td>
             <td>Friday</td>
             <td>Dec 13</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>6:10pm</td>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>7:10pm</td>
             <td>Friday</td>
             <td>Dec 13</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
         </tbody>
       </table>
 
       <h2>Common Final</h2>
-      <table border="1" cellpadding="1" cellspacing="1" summary="Common Final">
+      <table border="1" cellpadding summary="Common Final">
         <thead>
           <tr>
             <th scope="col">Exam Day</th>
@@ -381,22 +365,22 @@ const Schedule = () => {
           <tr>
             <td>Monday</td>
             <td>Dec 9</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>Tuesday</td>
             <td>Dec 10</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>Wednesday</td>
             <td>Dec 11</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
           <tr>
             <td>Thursday</td>
             <td>Dec 12</td>
-            <td>7:10pm –10:00pm</td>
+            <td>7:10pm - 10:00pm</td>
           </tr>
         </tbody>
       </table>
